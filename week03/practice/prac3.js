8739a58c2145e5a8b7e8a6e003f9cc8c44a38d1d
Ahn Jung-euk <woodencatty@gmail.com>
2017-09-25 14:36:54 +0900

    change

--- week03/practice/prac3.js
+++ week03/practice/prac3.js
@@ -2,47 +2,60 @@ const readline = require('readline');
 const crypto = require('crypto')
 const fs = require('fs');
 
-const rl = readline.createInterface({
+const r = readline.createInterface({
 	input: process.stdin,
-	output:process.stdout
+	output: process.stdout
 });
-
-var siteinfo = {
-	sitename : '',
-	URL : '',
-	id : '',
-	pwd : ''
-};
-
-if(process.argv[3] == '-store'){
-var cipher = crypto.createCipher('aes192', key);
 const key = 'gachon654321';
-console.log("----------insert info------------");
-	rl.question("sitename : ", (answer)=>{
-		siteinfo.sitename = answer;
-		rl.question("URL : http://", (answer)=>{
-			siteinfo.URL = answer;
-			rl.question("id : ", (answer)=>{
-				siteinfo.id = answer;
-				rl.question("pwd : ", (answer)=>{
-					siteinfo.pwd = answer;
-					chipher.update(siteinfo,'utf8','base64');
-					var chipherOutput = chipher.final('base64');
-					fs.writefile('siteinfo', chipherOutput, 'utf8', (error)=>{});
-				});
-			});
-		});
+
+if (process.argv[2] == '-store') {
+	var cipher = crypto.createCipher('aes192', key);
+
+	console.log("-------------------------------------");
+	console.log("사이트정보 등록프로그램 v1.0(암호화저장)");
+	console.log("-------------------------------------");
+
+	var menu = ['사이트명: ', '인터넷주소: ', '아이디: ', '비밀번호: '];
+	var content = ['', '', '', ''];
+	var index = 0;
+	const handlerForaLine = function (line) {
+		content[index] = line;
+		index++;
+		if (index >= 4) {
+			r.close();
+			var cipheredOutput = cipher.update(content.toString(), 'utf8', 'base64');
+			cipheredOutput += cipher.final('base64');
+
+			fs.writeFileSync('siteinfo', cipheredOutput.toString(), 'utf8');
+			console.log("암호화하여 siteinfo파일로 저장하였습니다.");
+			process.exit();
+		}
+		r.setPrompt(menu[index]);
+		r.prompt()
+	}
+
+	r.setPrompt(menu[index]);
+	r.prompt();
+	r.on('line', handlerForaLine);
+
+
+} else if (process.argv[2] == '-restore') {
+
+	console.log("-------------------------------------");
+	console.log("사이트정보 조회프로그램 v1.0(복호화조회)");
+	console.log("-------------------------------------");
+	var decipher = crypto.createDecipher('aes192', key);
+	fs.readFile('siteinfo', 'utf8', (error, readtext) => {
+		var decipherOutput = decipher.update(readtext, 'base64', 'utf8');
+		decipherOutput += decipher.final('utf8');
+		var Output = decipherOutput.split(',');
+		console.log("복호화하여 siteinfo파일에서 가져왔습니다.");
+		console.log("사이트명: " + Output[0]);
+		console.log("인터넷주소: " + Output[1]);
+		console.log("아이디: " + Output[2]);
+		console.log("비밀번호: " + Output[3]);
+
+		process.exit();
+
 	});
-
-}else if(process.argv[3] == '-restore'){
-var cryptoText;
-var decryptoText;
-const key = 'gachon654321';
-var decipher = crypto.createDecipher('aes192',key);
-fs.readFile('siteinfo', 'utf8', (error, readtext)=>{
-	     cryptoText = readtext;
-	     decipher.update(cryptoText, 'base64', 'utf8');
-	     decryptoText = decipher.final('utf8');
-	     console.log(decryptoText);
-});
 }
