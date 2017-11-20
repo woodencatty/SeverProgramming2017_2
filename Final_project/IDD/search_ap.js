c515141c5929b059562bd66a0b08730d2598663c
Ahn Jung-euk <woodencatty@gmail.com>
2017-11-20 14:57:34 +0900

    emergency fix

---
+++ Final_project/IDD/search_ap.js
@@ -0,0 +1,23 @@
+const sendData = require('./rest_api.js')   //포스터기기 연결 모듈 import
+
+const scanner = require('node-wifi-scanner');
+
+
+module.exports = {
+    searchAPD: (apName, TargetRSSI) => {
+        scanner.scan((err, networks) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            if (networks.ssid == apName) {
+                if (networks.rssi < TargetRSSI) {
+                    sendData.SubmitIDDname(ID, () => {
+                        sendData.SubmitUserExercise(exercise);
+                    })
+                }
+            }
+        });
+    }
+
+}
