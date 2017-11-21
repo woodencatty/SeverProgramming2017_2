77be39229621bf517253f5221849915d788f062f
woodencatty <woodencatty@gmail.com>
2017-11-21 09:31:48 +0900

    testcode

--- Final_project/APD/routes/hotSpot.js
+++ Final_project/APD/routes/hotSpot.js
@@ -1,20 +1,9 @@
-var SoftAPSetup = require('../index');
-var config = require('./config');
-var path = require('path');
+var SoftAPSetup = require('softap-setup');
 
 var sap = new SoftAPSetup();
-
-if(!config.get('ssid')) {
-
-	console.log("* Please specify the ssid of the AP with which you wish to connect your device...");
-	console.log("Example: %s %s --ssid BestWiFiNetworkEver --password SuperSecretPassword --security wpa2_mixed",
-		process.argv[0], path.relative(process.cwd(), __filename));
-	process.exit(1);
-}
-
-function deviceInfo() {
-	console.log("Obtaining device information...");
-	sap.deviceInfo(claim);
-}
-
-deviceInfo();
+sap.scan(callback);
+function callback(err, dat) {
+    if(err) { throw err; }
+    console.log("Networks Identified:");
+    console.log(dat);
+};
