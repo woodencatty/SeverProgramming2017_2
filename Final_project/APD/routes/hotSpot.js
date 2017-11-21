00da5038f8af75e2a4cb4d034072f9ba6b585bec
woodencatty <woodencatty@gmail.com>
2017-11-21 11:11:51 +0900

    hotspot

---
+++ Final_project/APD/routes/hotSpot.js
@@ -0,0 +1,37 @@
+var hotspot = require('node-hotspot');
+
+var opts = {
+   ssid: 'APD', 
+   password: '1q2w3e4r', 
+   force: true, // (optional)  if hosting a network already turn it off and run ours. 
+   adaptor: 'Ethernet' // (optional / false) name of adaptor to have ICS (Internet Connection Sharing) share internet from, passing false disables ICS all together - if non givin node-hotspot will attempt to find currently connected adaptor automatically 
+};
+
+hotspot.enable(opts)
+   .then(function() {
+       console.log('Hotspot Enabled')
+   })
+   .catch(function(e) {
+       Console.log('Something went wrong; Perms?', e)
+   });
+
+   process.on('SIGINT', () => {
+hotspot.disable(opts)
+.then(function() {
+    console.log('Hotspot disabled')
+})
+.catch(function(e) {
+    Console.log('Something went wrong; Perms?', e)
+});
+
+process.exit(1);
+  });
+
+  setInterval(()=>{
+
+    hotspot.status(opts)
+    .then(function(status) {
+        console.log('Hotspot status: ' + status) //status contains clients object and state 
+    });
+ 
+  }, 1000);
