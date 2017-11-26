455a81ec619a2b3979bc79d715f18051c69ce6f5
woodencatty <woodencatty@gmail.com>
2017-11-27 08:56:26 +0900

    change sensor

--- Final_project/IDD/Test/Accel.js
+++ Final_project/IDD/Test/Accel.js
@@ -1,18 +1,5 @@
 const ADXL345 = require('adxl345-sensor');  //가속도 센서 모듈
 const adxl345 = new ADXL345();              //가속도 센서 정의
-
-var data = require('./model.js');
-var ml = require('machine_learning');
-var accel = require('./sensor.js');
-
-var dt = new ml.DecisionTree({
-data : data.data,
-result : data.result
-});
-
-dt.build();
-dt.prune(1.0); // 1.0 : mingain.                    
-
 // 가속도 센서 초기화
 adxl345.init()
     .then(() => {
@@ -25,9 +12,8 @@ setInterval(() => {
         .then((acceleration) => {
             //가속도값(X, Y, Z) 반환
             console.log(acceleration.x + " " + acceleration.y + " " + acceleration.z);
-            console.log(dt.classify([acceleration.x.toFixed(3), acceleration.y.toFixed(3), acceleration.z.toFixed(3)]));            
         })
         .catch((err) => {
             console.log(`ADXL345 read error: ${err}`);
         });
-}, 1000)
+}, 500)
