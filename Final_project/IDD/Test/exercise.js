0a91853ddba26fee607edff4d5d362a69afc2cc3
woodencatty <woodencatty@gmail.com>
2017-11-27 08:44:35 +0900

    test

--- Final_project/IDD/Test/exercise.js
+++ Final_project/IDD/Test/exercise.js
@@ -9,11 +9,12 @@ result : data.result
 
 dt.build();
 
+dt.prune(1.0); // 1.0 : mingain.                    
+
 setInterval(()=>{
     getExercisecallback = (AccelX, AccelY, AccelZ) => {
         console.log((AccelX, AccelY, AccelZ));
         console.log(dt.classify([AccelX.toFixed(3), AccelY.toFixed(3), AccelZ.toFixed(3)]));
-            dt.prune(1.0); // 1.0 : mingain.                    
         }
  accel.getAccel(getExercisecallback);
 },1000);
