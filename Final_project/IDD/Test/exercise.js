404bf2d5f2b02c2e53b45dc27123ccc8ab540e0a
woodencatty <woodencatty@gmail.com>
2017-11-27 08:38:45 +0900

    testcode

---
+++ Final_project/IDD/Test/exercise.js
@@ -0,0 +1,21 @@
+var data = require('./model.js');
+var ml = require('machine_learning');
+var accel = require('./sensor.js');
+
+var dt = new ml.DecisionTree({
+data : data.data,
+result : data.result
+});
+
+dt.build();
+
+setInterval(()=>{
+    getExercisecallback = (AccelX, AccelY, AccelZ) => {
+        console.log((AccelX, AccelY, AccelZ));
+        console.log(dt.classify([AccelX.toFixed(3), AccelY.toFixed(3), AccelZ.toFixed(3)]));
+            callback(dt.classify([AccelX.toFixed(3), AccelY.toFixed(3), AccelZ.toFixed(3)]));
+            dt.prune(5.0); // 1.0 : mingain.                    
+        }
+ accel.getAccel(getExercisecallback);
+},1000);
+           
