da6149e89b9e032ec44720926334417387d96ec6
woodencatty <woodencatty@gmail.com>
2017-11-27 09:13:35 +0900

    total fix

--- Final_project/IDD/exercise.js
+++ Final_project/IDD/exercise.js
@@ -3,23 +3,23 @@ var ml = require('machine_learning');
 var accel = require('./sensor.js');
 
 var dt = new ml.DecisionTree({
-data : data.data,
-result : data.result
+    data: data.data,
+    result: data.result
 });
 
-dt.build();
 
 // dt.print(); // Show Trees
 
-dt.prune(1.0); // 1.0 : mingain.
 
 module.exports = {
-        getExercise : (callback)=>{
-            getExercisecallback = (AccelX, AccelY, AccelZ) => {
-                console.log(dt.classify([AccelX.toFixed(3), AccelY.toFixed(3), AccelZ.toFixed(3)]));
-                    callback(dt.classify([AccelX.toFixed(3), AccelY.toFixed(3), AccelZ.toFixed(3)]));
-                            }
-         accel.getAccel(getExercisecallback);
+    getExercise: (callback) => {
+        getExercisecallback = (AccelX, AccelY, AccelZ) => {
+            dt.build();
+            console.log(dt.classify([AccelX.toFixed(3), AccelY.toFixed(3), AccelZ.toFixed(3)]));
+            callback(dt.classify([AccelX.toFixed(3), AccelY.toFixed(3), AccelZ.toFixed(3)]));
+            dt.prune(1.0); // 1.0 : mingain.            
         }
-    
+        accel.getAccel(getExercisecallback);
+    }
+
 }
