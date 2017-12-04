806b66ad03ad23ce0a34ba2f89d569e97e3f6436
woodencatty <woodencatty@gmail.com>
2017-12-04 18:27:55 +0900

    test2

--- Final_project/Server/routes/index.js
+++ Final_project/Server/routes/index.js
@@ -542,8 +542,8 @@ router.get('/patient_manage', (req, res, next) => {
         } else {
             client.query('SELECT * FROM exercise', (err, exercise_data) => {
             client.query('SELECT * FROM patient', (err, user_data) => {
-                var date_arr = ["may 1", "may 2"];
-                var exer_arr = [10, 10];
+                var date_arr = "['may 1', 'may 2']";
+                var exer_arr = "[10, 10]";
                 req.session.now = (new Date()).toUTCString();
                 res.render('patient_manage', {
                     name: req.session.user_name,
